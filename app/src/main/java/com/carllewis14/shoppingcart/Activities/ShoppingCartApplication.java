38d58256fdc34200748ad01788f4e4fc01355eea
carl Lewis <carl.lewis@incisivemedia.com>
2017-12-05 18:35:39 +0000

    Dagger component finished

--- app/src/main/java/com/carllewis14/shoppingcart/Activities/ShoppingCartApplication.java
+++ app/src/main/java/com/carllewis14/shoppingcart/Activities/ShoppingCartApplication.java
@@ -1,10 +1,40 @@
 package com.carllewis14.shoppingcart.Activities;
 
 import android.app.Application;
+import dagger.Component;
+import com.carllewis14.shoppingcart.Dependecies.AppComponent;
+import com.carllewis14.shoppingcart.Dependecies.AppModule;
 
 /**
  *
  */
 
 public class ShoppingCartApplication extends Application {
+
+    private static ShoppingCartApplication instance = new ShoppingCartApplication();
+    private static AppComponent appComponent;
+
+
+    public static ShoppingCartApplication getInstance() {
+        return instance;
+    }
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getAppComponent();
+    }
+
+    public AppComponent getAppComponent() {
+        if (appComponent == null){
+            appComponent = DaggerAppComponent.builder()
+                    .appModule(new AppModule(this))
+                    .build();
+        }
+        return appComponent;
+    }
+
+
 }
--- app/src/main/java/com/carllewis14/shoppingcart/Presenter/ProductPresenter.java
+++ app/src/main/java/com/carllewis14/shoppingcart/Presenter/ProductPresenter.java
@@ -1,8 +1,45 @@
 package com.carllewis14.shoppingcart.Presenter;
 
+import com.carllewis14.shoppingcart.Activities.ShoppingCart;
+import com.carllewis14.shoppingcart.Activities.ShoppingCartApplication;
+import com.carllewis14.shoppingcart.Model.LineItem;
+import com.carllewis14.shoppingcart.Model.Product;
+
+import javax.inject.Inject;
+
 /**
  * Presenter for products
  */
 
 public class ProductPresenter {
+
+    //create a class member variable for
+    //shopping cart to be injected
+    @Inject
+    ShoppingCart mCart;
+
+    public ProductPresenter() {
+        //injection occurs here
+        ShoppingCartApplication.getInstance().getAppComponent().inject(this);
+    }
+
+    //
+    public void onItemQuantityChanged(LineItem item, int qty) {
+        mCart.updateItemQty(item, qty);
+    }
+
+    //Another example of using the shopping cart
+    public void onAddToCartButtonClicked(Product product) {
+        //perform add to checkout button here
+        LineItem item = new LineItem(product, 1);
+        mCart.addItemToCart(item);
+    }
+
+    public void onClearButtonClicked() {
+        mCart.clearShoppingCart();
+    }
+
+    public void onDeleteItemButtonClicked(LineItem item) {
+        mCart.removeItemFromCart(item);
+    }
 }

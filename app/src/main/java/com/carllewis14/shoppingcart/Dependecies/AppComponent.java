4d5eae03ca8ee518b0ab858fc1e5555dfaac2bef
carl Lewis <carl.lewis@incisivemedia.com>
2017-12-05 01:53:19 +0000

    added update to lineitem pojo and added Context to return application

--- app/src/main/java/com/carllewis14/shoppingcart/Dependecies/AppComponent.java
+++ app/src/main/java/com/carllewis14/shoppingcart/Dependecies/AppComponent.java
@@ -1,10 +1,27 @@
 package com.carllewis14.shoppingcart.Dependecies;
 
+import com.carllewis14.shoppingcart.Activities.MainActivity;
+import com.carllewis14.shoppingcart.Presenter.ProductPresenter;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * This interface does two things
  * lists available Dagger Modules
  * lists injectable targets
  */
+@Singleton
+@Component(
+        modules = {
+                AppModule.class,
+                ShoppingCartModule.class
+        }
+)
 
 public interface AppComponent {
+
+    void inject(ProductPresenter productPresenter);
+    void inject(MainActivity mainActivity);
 }
--- app/src/main/java/com/carllewis14/shoppingcart/Dependecies/ShoppingCartModule.java
+++ app/src/main/java/com/carllewis14/shoppingcart/Dependecies/ShoppingCartModule.java
@@ -1,8 +1,31 @@
 package com.carllewis14.shoppingcart.Dependecies;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.carllewis14.shoppingcart.Activities.ShoppingCart;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * Provides the instances for the ShoppingCart
  */
-
+@Module
 public class ShoppingCartModule {
+
+    @Provides @Singleton
+    SharedPreferences providesSharedPref(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    @Provides @Singleton
+    ShoppingCart providesShoppingCart(SharedPreferences sharedPreferences){
+        return new ShoppingCart(sharedPreferences);
+    }
+
+
 }
